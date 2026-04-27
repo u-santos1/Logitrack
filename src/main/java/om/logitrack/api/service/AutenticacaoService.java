8d23833bb9fa503e3ed345534d5256c74e0a6544
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-04-27 14:11:47 -0300

    feat: implementa servicos de autenticacao e token JWT
    Cria o AutenticacaoService para busca de usuarios no banco. Adiciona TokenService integrado com a biblioteca auth0/java-jwt para geracao e validacao de tokens.

--- src/main/java/om/logitrack/api/service/AutenticacaoService.java
+++ src/main/java/om/logitrack/api/service/AutenticacaoService.java
@@ -1,4 +1,19 @@
 package om.logitrack.api.service;
 
-public class AutenticacaoService {
+import lombok.RequiredArgsConstructor;
+import om.logitrack.api.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return usuarioRepository.findByEmail(username);
+    }
 }
