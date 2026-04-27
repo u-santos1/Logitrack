2030b0d088a38a69b1493ad402d4177c086e5da8
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-04-27 14:13:51 -0300

    feat: adiciona entidade Usuario e configuracao do UserDetails
    Mapeia a entidade no banco de dados e implementa a interface do Spring Security para padronizacao de login. Cria o UsuarioRepository para consultas.

--- src/main/java/om/logitrack/api/repository/UsuarioRepository.java
+++ src/main/java/om/logitrack/api/repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package om.logitrack.api.repository;
 
-public interface UsuarioRepository {
+import om.logitrack.api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByEmail(String email);
 }
