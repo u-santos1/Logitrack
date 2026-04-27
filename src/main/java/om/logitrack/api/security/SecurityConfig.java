307d7bcea8d6a8aef0cfbac616bb1ad9c5c56558
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-04-27 14:12:47 -0300

    chore: configura infraestrutura do Spring Security
    Adiciona SecurityConfigurations. Configura a aplicacao como STATELESS, exporta o AuthenticationManager e o BCryptPasswordEncoder. Define rotas publicas e privadas.

--- src/main/java/om/logitrack/api/security/SecurityConfig.java
+++ src/main/java/om/logitrack/api/security/SecurityConfig.java
@@ -4,10 +4,13 @@ package om.logitrack.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,5 +27,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/empresas").permitAll()
                         .anyRequest().authenticated()
                         ).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
